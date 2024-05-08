3f425264349a0ae8e3a9dd69bb9f5d1075dd46ab
Byron Jones <byron@glob.com.au>
2024-05-08 14:25:06 +0800

    improve sleeping message when multiple players sleep

--- src/main/java/au/com/glob/clodmc/modules/player/Sleep.java
+++ src/main/java/au/com/glob/clodmc/modules/player/Sleep.java
@@ -2,6 +2,7 @@ package au.com.glob.clodmc.modules.player;
 
 import au.com.glob.clodmc.ClodMC;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
@@ -20,12 +21,31 @@ public class Sleep implements Listener {
   public void onTimeSkip(TimeSkipEvent event) {
     Collection<? extends Player> players = ClodMC.instance.getServer().getOnlinePlayers();
     List<String> sleeping =
-        players.stream().filter(LivingEntity::isSleeping).map(Player::getName).toList();
+        new java.util.ArrayList<>(
+            players.stream()
+                .filter(LivingEntity::isSleeping)
+                .map(Player::getName)
+                .sorted(Comparator.naturalOrder())
+                .toList());
     if (sleeping.isEmpty()) {
       return;
     }
 
-    String message = "<grey>" + String.join(",", sleeping) + " skipped the night</grey>";
+    String names;
+    if (sleeping.size() == 1) {
+      names = sleeping.get(0);
+    } else if (sleeping.size() == 2) {
+      names = String.join(" and ", sleeping);
+    } else {
+      names =
+          String.join(
+              ", and ",
+              List.of(
+                  String.join(", ", sleeping.subList(0, sleeping.size() - 1)),
+                  sleeping.get(sleeping.size() - 1)));
+    }
+    String message = "<grey>" + names + " skipped the night</grey>";
+
     for (Player player : players) {
       if (player.getWorld().getEnvironment() == World.Environment.NORMAL) {
         player.sendRichMessage(message);
