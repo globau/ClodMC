c13ebec48282959f243c03d0715026b85bd60ecc
Byron Jones <byron@glob.com.au>
2024-07-27 11:33:57 +0800

    move module init into own class

--- src/main/java/au/com/glob/clodmc/ClodMC.java
+++ src/main/java/au/com/glob/clodmc/ClodMC.java
@@ -1,35 +1,8 @@
 package au.com.glob.clodmc;
 
 import au.com.glob.clodmc.modules.Module;
+import au.com.glob.clodmc.modules.ModuleRegistry;
 import au.com.glob.clodmc.modules.SimpleCommand;
-import au.com.glob.clodmc.modules.bluemap.BlueMap;
-import au.com.glob.clodmc.modules.gateways.Gateways;
-import au.com.glob.clodmc.modules.homes.DelHomeCommand;
-import au.com.glob.clodmc.modules.homes.HomeCommand;
-import au.com.glob.clodmc.modules.homes.Homes;
-import au.com.glob.clodmc.modules.homes.HomesCommand;
-import au.com.glob.clodmc.modules.homes.SetHomeCommand;
-import au.com.glob.clodmc.modules.interactions.Decorations;
-import au.com.glob.clodmc.modules.interactions.FastLeafDecay;
-import au.com.glob.clodmc.modules.interactions.NamedStorage;
-import au.com.glob.clodmc.modules.inventory.AdminInvCommand;
-import au.com.glob.clodmc.modules.inventory.InventorySort;
-import au.com.glob.clodmc.modules.mobs.BetterDrops;
-import au.com.glob.clodmc.modules.mobs.PreventMobGriefing;
-import au.com.glob.clodmc.modules.mobs.PreventMobSpawn;
-import au.com.glob.clodmc.modules.player.AFK;
-import au.com.glob.clodmc.modules.player.BackCommand;
-import au.com.glob.clodmc.modules.player.GameModeCommand;
-import au.com.glob.clodmc.modules.player.InviteCommand;
-import au.com.glob.clodmc.modules.player.OfflineMessages;
-import au.com.glob.clodmc.modules.player.OpAlerts;
-import au.com.glob.clodmc.modules.player.PlayerTracker;
-import au.com.glob.clodmc.modules.player.SeenCommand;
-import au.com.glob.clodmc.modules.player.Sleep;
-import au.com.glob.clodmc.modules.player.SpawnCommand;
-import au.com.glob.clodmc.modules.player.WelcomeBook;
-import au.com.glob.clodmc.modules.server.CircularWorldBorder;
-import au.com.glob.clodmc.modules.server.RequiredPlugins;
 import au.com.glob.clodmc.util.MaterialUtil;
 import au.com.glob.clodmc.util.PlayerLocation;
 import java.io.File;
@@ -37,9 +10,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.StringJoiner;
 import java.util.logging.Level;
 import java.util.stream.Stream;
@@ -59,8 +30,8 @@ public final class ClodMC extends JavaPlugin implements Listener {
   public static @NotNull ClodMC instance;
 
   public static boolean sanityChecked;
-  private final @NotNull Map<Class<? extends Module>, Module> modules = new HashMap<>();
   private boolean shuttingDown;
+  private final @NotNull ModuleRegistry moduleRegistry = new ModuleRegistry();
 
   // init
 
@@ -82,51 +53,7 @@ public final class ClodMC extends JavaPlugin implements Listener {
     MaterialUtil.init();
     Bukkit.getPluginManager().registerEvents(this, this);
 
-    // core - used by other modules
-    this.register(new OpAlerts());
-
-    // gateways
-    this.register(new Gateways());
-
-    // homes
-    this.register(new Homes());
-    this.register(new DelHomeCommand());
-    this.register(new HomeCommand());
-    this.register(new HomesCommand());
-    this.register(new SetHomeCommand());
-
-    // interactions
-    this.register(new Decorations());
-    this.register(new FastLeafDecay());
-    this.register(new NamedStorage());
-
-    // inventory
-    this.register(new AdminInvCommand());
-    this.register(new InventorySort());
-
-    // mobs
-    this.register(new BetterDrops());
-    this.register(new PreventMobGriefing());
-    this.register(new PreventMobSpawn());
-
-    // player
-    this.register(new AFK());
-    this.register(new BackCommand());
-    this.register(new GameModeCommand());
-    this.register(new InviteCommand());
-    this.register(new OfflineMessages());
-    this.register(new PlayerTracker());
-    this.register(new SeenCommand());
-    this.register(new Sleep());
-    this.register(new SpawnCommand());
-    this.register(new WelcomeBook());
-
-    // server
-    this.register(new CircularWorldBorder());
-    this.register(new RequiredPlugins());
-
-    // bluemap
-    this.register(new BlueMap());
+    this.moduleRegistry.registerAll();
 
     // load configs after sanity checking
     try {
@@ -139,7 +66,8 @@ public final class ClodMC extends JavaPlugin implements Listener {
       this.shuttingDown = true;
       return;
     }
-    for (Module module : this.modules.values()) {
+
+    for (Module module : this.moduleRegistry.all()) {
       module.loadConfig();
     }
   }
@@ -189,34 +117,8 @@ public final class ClodMC extends JavaPlugin implements Listener {
     }
   }
 
-  private void register(@NotNull Module module) {
-    String dependsOn = module.dependsOn();
-    if (dependsOn != null && !Bukkit.getPluginManager().isPluginEnabled(dependsOn)) {
-      logWarning(
-          "Cannot load "
-              + module.getClass().getSimpleName()
-              + ": depends on plugin "
-              + dependsOn
-              + " which is not enabled");
-      return;
-    }
-
-    this.modules.put(module.getClass(), module);
-
-    if (module instanceof Listener listener) {
-      Bukkit.getServer().getPluginManager().registerEvents(listener, this);
-    }
-
-    if (module instanceof SimpleCommand command) {
-      this.getServer().getCommandMap().register("clod-mc", command);
-    }
-  }
-
-  @SuppressWarnings("unchecked")
   public static @NotNull <T extends Module> T getModule(@NotNull Class<T> moduleClass) {
-    Module module = instance.modules.get(moduleClass);
-    assert module != null;
-    return (T) module;
+    return instance.moduleRegistry.get(moduleClass);
   }
 
   @Override
---
+++ src/main/java/au/com/glob/clodmc/modules/ModuleRegistry.java
@@ -0,0 +1,134 @@
+package au.com.glob.clodmc.modules;
+
+import au.com.glob.clodmc.ClodMC;
+import au.com.glob.clodmc.modules.bluemap.BlueMap;
+import au.com.glob.clodmc.modules.gateways.Gateways;
+import au.com.glob.clodmc.modules.homes.DelHomeCommand;
+import au.com.glob.clodmc.modules.homes.HomeCommand;
+import au.com.glob.clodmc.modules.homes.Homes;
+import au.com.glob.clodmc.modules.homes.HomesCommand;
+import au.com.glob.clodmc.modules.homes.SetHomeCommand;
+import au.com.glob.clodmc.modules.interactions.Decorations;
+import au.com.glob.clodmc.modules.interactions.FastLeafDecay;
+import au.com.glob.clodmc.modules.interactions.NamedStorage;
+import au.com.glob.clodmc.modules.inventory.AdminInvCommand;
+import au.com.glob.clodmc.modules.inventory.InventorySort;
+import au.com.glob.clodmc.modules.mobs.BetterDrops;
+import au.com.glob.clodmc.modules.mobs.PreventMobGriefing;
+import au.com.glob.clodmc.modules.mobs.PreventMobSpawn;
+import au.com.glob.clodmc.modules.player.AFK;
+import au.com.glob.clodmc.modules.player.BackCommand;
+import au.com.glob.clodmc.modules.player.GameModeCommand;
+import au.com.glob.clodmc.modules.player.InviteCommand;
+import au.com.glob.clodmc.modules.player.OfflineMessages;
+import au.com.glob.clodmc.modules.player.OpAlerts;
+import au.com.glob.clodmc.modules.player.PlayerTracker;
+import au.com.glob.clodmc.modules.player.SeenCommand;
+import au.com.glob.clodmc.modules.player.Sleep;
+import au.com.glob.clodmc.modules.player.SpawnCommand;
+import au.com.glob.clodmc.modules.player.WelcomeBook;
+import au.com.glob.clodmc.modules.server.CircularWorldBorder;
+import au.com.glob.clodmc.modules.server.RequiredPlugins;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.jetbrains.annotations.NotNull;
+
+public class ModuleRegistry {
+  private final @NotNull Map<Class<? extends Module>, Module> modules = new HashMap<>();
+
+  private void register(@NotNull Class<? extends Module> moduleClass) {
+    Module module;
+    try {
+      module = moduleClass.getDeclaredConstructor().newInstance();
+    } catch (InstantiationException
+        | IllegalAccessException
+        | InvocationTargetException
+        | NoSuchMethodException e) {
+      throw new RuntimeException(e);
+    }
+
+    String dependsOn = module.dependsOn();
+    if (dependsOn != null && !Bukkit.getPluginManager().isPluginEnabled(dependsOn)) {
+      ClodMC.logWarning(
+          "Cannot load "
+              + module.getClass().getSimpleName()
+              + ": depends on plugin "
+              + dependsOn
+              + " which is not enabled");
+      return;
+    }
+
+    this.modules.put(moduleClass, module);
+
+    if (module instanceof Listener listener) {
+      Bukkit.getServer().getPluginManager().registerEvents(listener, ClodMC.instance);
+    }
+
+    if (module instanceof SimpleCommand command) {
+      Bukkit.getServer().getCommandMap().register("clod-mc", command);
+    }
+  }
+
+  public void registerAll() {
+    // core - used by other modules
+    this.register(OpAlerts.class);
+
+    // gateways
+    this.register(Gateways.class);
+
+    // homes
+    this.register(Homes.class);
+    this.register(DelHomeCommand.class);
+    this.register(HomeCommand.class);
+    this.register(HomesCommand.class);
+    this.register(SetHomeCommand.class);
+
+    // interactions
+    this.register(Decorations.class);
+    this.register(FastLeafDecay.class);
+    this.register(NamedStorage.class);
+
+    // inventory
+    this.register(AdminInvCommand.class);
+    this.register(InventorySort.class);
+
+    // mobs
+    this.register(BetterDrops.class);
+    this.register(PreventMobGriefing.class);
+    this.register(PreventMobSpawn.class);
+
+    // player
+    this.register(AFK.class);
+    this.register(BackCommand.class);
+    this.register(GameModeCommand.class);
+    this.register(InviteCommand.class);
+    this.register(OfflineMessages.class);
+    this.register(PlayerTracker.class);
+    this.register(SeenCommand.class);
+    this.register(Sleep.class);
+    this.register(SpawnCommand.class);
+    this.register(WelcomeBook.class);
+
+    // server
+    this.register(CircularWorldBorder.class);
+    this.register(RequiredPlugins.class);
+
+    // bluemap
+    this.register(BlueMap.class);
+  }
+
+  @SuppressWarnings("unchecked")
+  public @NotNull <T extends Module> T get(@NotNull Class<T> moduleClass) {
+    Module module = this.modules.get(moduleClass);
+    assert module != null;
+    return (T) module;
+  }
+
+  public @NotNull Collection<Module> all() {
+    return this.modules.values();
+  }
+}
