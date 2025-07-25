08a013a399fa7f1f9a78c8986dcf9f3e15242f62
Byron Jones <byron@glob.com.au>
2025-07-25 11:50:02 +0800

    migrate test-permissions.py to FilePermissionsCheck.java

---
+++ src/main/java/au/com/glob/checks/FilePermissionsCheck.java
@@ -0,0 +1,19 @@
+package au.com.glob.checks;
+
+import com.puppycrawl.tools.checkstyle.api.AbstractFileSetCheck;
+import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
+import com.puppycrawl.tools.checkstyle.api.FileText;
+import java.io.File;
+import org.jspecify.annotations.NullMarked;
+
+/** checkstyle check that ensures files in src/main directory don't have execute permissions. */
+@NullMarked
+public class FilePermissionsCheck extends AbstractFileSetCheck {
+
+  @Override
+  protected void processFiltered(File file, FileText fileText) throws CheckstyleException {
+    if (file.canExecute() && CheckUtils.isRelativeTo(file.getAbsolutePath(), "src/main")) {
+      this.log(0, "bad file permissions (+x): " + file.getPath());
+    }
+  }
+}
