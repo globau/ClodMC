04c8fa3ccac2f6e733093135ff49dc57456f5585
Byron Jones <byron@glob.com.au>
2025-08-21 19:42:00 +0800

    only show current release's changes in readme

--- src/main/java/au/com/glob/clodmc/build/ReleaseReadme.java
+++ src/main/java/au/com/glob/clodmc/build/ReleaseReadme.java
@@ -2,33 +2,25 @@ package au.com.glob.clodmc.build;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/** augment README.md with a list of recent changes from the commit log */
+/** augment README.md with a changes since the last release */
 @SuppressWarnings("NullabilityAnnotations")
 public class ReleaseReadme {
-  // generate readme with recent changes from git log
   public static void main(String[] args) {
     Util.mainWrapper(
         () -> {
           String rootPath = Util.capture("git", "rev-parse", "--show-toplevel").trim();
 
           System.out.println(Files.readString(Path.of(rootPath, "README.md")));
-          System.out.println("## Recent Changes\n");
-
-          int tagCount = 0;
-          String lastTag = null;
+          System.out.println("## Changes\n");
 
           String commitLog = Util.capture("git", "log", "--pretty=format:[%d] %h %s");
-          String[] commitLines = commitLog.split("\n", -1);
-
-          for (String commitLine : commitLines) {
+          for (String commitLine : commitLog.split("\n", -1)) {
             commitLine = commitLine.trim();
-            Matcher matcher = Pattern.compile("^\\[([^]]*)] (\\S+) (.+)$").matcher(commitLine);
 
+            Matcher matcher = Pattern.compile("^\\[([^]]*)] (\\S+) (.+)$").matcher(commitLine);
             if (!matcher.matches()) {
               throw new RuntimeException("Failed to parse commit line: %s".formatted(commitLine));
             }
@@ -37,27 +29,8 @@ public class ReleaseReadme {
             String sha = matcher.group(2);
             String desc = matcher.group(3);
 
-            // extract tags
-            List<String> tags = new ArrayList<>();
-            if (!meta.isEmpty()) {
-              String[] parts = meta.replaceAll("^\\(|\\)$", "").split(", ", -1);
-              for (String part : parts) {
-                if (part.startsWith("tag: v")) {
-                  tags.add(part.substring(5)); // remove "tag: " prefix
-                }
-              }
-            }
-
-            String tag = tags.isEmpty() ? null : tags.getFirst();
-            if (tag != null) {
-              tagCount++;
-              if (tagCount == 6) {
-                break;
-              }
-              if (!tag.equals(lastTag)) {
-                System.out.printf("%n#### %s%n", tag);
-                lastTag = tag;
-              }
+            if (meta.startsWith("(tag:")) {
+              break;
             }
 
             System.out.printf("- %s %s%n", sha, desc);
